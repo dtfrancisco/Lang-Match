ebc538c064906e7fdfe06e5ae3e49d90f2007cd0
Damon <damontfrancisco@yahoo.com>
2019-03-04 18:45:41 -0600

    Added flask-login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,38 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
+from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/browse')
+@login_required
 def browse():
-    return render_template('browse.html')
+    return render_template('browse.html', title='Browse')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('browse'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login requested for {form.username.data}, remember me={form.remember_me.data}')
-        return redirect(url_for('browse'))
-    return render_template('login.html', form=form)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash(f'Invalid username or password')
+            return redirect(url_for('browse'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        # Set next_page to browse if not set and also set it to browse in case external website is provided
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('browse')
+        return redirect(next_page)
+    return render_template('login.html', form=form, title='Login')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('browse'))
